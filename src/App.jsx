44f6349cbb1c00ff57c8953f2d8175660fe8ab5e
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-05-16 10:06:57 +0200

    consuming the context

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
 
   return (
     // CartContext.Provider is a wrapper component that provides the context to its children, the .Provider component takes a value prop that is the value of the context, and means that all children of the CartContext.Provider component will have access to the context value
-    <CartContext.Provider>
+    <CartContext.Provider value={{ items: [] }}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
